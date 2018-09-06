725740cdde6e74089ace700daeebf029fb96580c
Zheng Fu <zf38@cornell.edu>
2018-09-06 14:18:33 -0500

    WIP for picatic controller using rest-client

--- app/controllers/picatic_controller.rb
+++ app/controllers/picatic_controller.rb
@@ -1,4 +1,9 @@
 class PicaticController < ApplicationController
   def create_event
+    api_key = 'Bearer sk_live_86c61624a690d4c6cbcdb559344ac108'
+    url = 'https://api.picatic.com/v2/user/me'
+    headers = {authorization: api_key}
+    res = RestClient.get(url, {authorization: api_key})
+    put res
   end
 end
