6e735f8236329096887f3358c277b15234319295
Zheng Fu <zf38@cornell.edu>
2018-09-07 07:32:20 -0500

    finalized eventbrite create event method skeleton

--- app/controllers/eventbrite_controller.rb
+++ app/controllers/eventbrite_controller.rb
@@ -1,19 +1,24 @@
 class EventbriteController < ApplicationController
   require 'eventbrite_sdk'
   EventbriteSDK.token = "3P7L3QRIMRRPMXAWSC42"
-
-  def create_venue(params)
-    venue = EventbriteSDK::Venue.build('name' => params[:name],
-                                       'address' => params[:address])
+  def initialize
+    @organizer = EventbriteSDK::Organizer.retrieve(id: 17808905770)
   end
 
-  def create_event(params)
-    event = EventbriteSDK::Event.build('name.html' => params[:name],
-                                       'start.utc' => params[:start_time],
-                                       'start.timezone' => params[:start_timezone],
-                                       'end.utc' => params[:end_time],
-                                       'end.timezone' => params[:end_timezone],
-                                       'currency' => params[:currency])
+  def create_event
+    test_venue = EventbriteSDK::Venue.build('name' => 'test venue',
+                                            'address.address_1' => '123 test address rd',
+                                            'organizer_id' => @organizer.id)
+    test_venue.save
+
+    event = EventbriteSDK::Event.build('name.html' => 'test event',
+                                       'start.utc' => '2018-09-12T10:00:00Z',
+                                       'start.timezone' => 'America/Los_Angeles',
+                                       'end.utc' => '2018-09-12T13:00:00Z',
+                                       'end.timezone' => 'America/Los_Angeles',
+                                       'currency' => 'USD',
+                                       'venue_id' => test_venue.id,
+                                       'organizer_id' => @organizer.id)
     event.list!
   end
 end
